3e62082c467df7c14b1037a17a12747ad0ee91a7
thiagoos16 <tos@icomp.ufam.edu.br>
2021-04-19 23:32:00 -0400

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-      <h1>dtmoney</h1>
+    <>
+      <Header/>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
