7ec9fe1289ca64deda96ebae903ff8bbfdfe3fd6
thiago.oliveira <thiago.oliveira@intera.org.br>
2021-05-19 21:26:18 -0400

    add modal to new transaction with react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import Modal from 'react-modal';
 
 import { GlobalStyle } from './styles/global';
 
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard/>
+      
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+
       <GlobalStyle />
     </>
   );
