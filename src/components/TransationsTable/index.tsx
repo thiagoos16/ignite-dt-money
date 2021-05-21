146c53ae88a32a24953da5073b8968a6dd3fc37b
thiago.oliveira <thiago.oliveira@intera.org.br>
2021-05-20 23:47:21 -0400

    add create in context

--- src/components/TransationsTable/index.tsx
+++ src/components/TransationsTable/index.tsx
@@ -3,7 +3,7 @@ import { Container } from "./styles";
 import { TransactionsContext } from '../../TransactionsContext';
 
 export function TransationsTable() {
-    const transactions = useContext(TransactionsContext);
+    const { transactions } = useContext(TransactionsContext);
 
     return (
         <Container>
